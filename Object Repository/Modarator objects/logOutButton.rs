e76281ba92c956da8c1e0c85799130ea58843572
Mahesh <mahesh.pentyala@qualitlabs.com>
2019-11-19 18:09:38 +0530

    added logout featur

---
+++ Object Repository/Modarator objects/logOutButton.rs
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>logOutButton</name>
+   <tag></tag>
+   <elementGuidId>db57b7f9-fac2-4f46-b2cd-ad6907273e41</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//*[@class = 'logout']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>logout</value>
+   </webElementProperties>
+</WebElementEntity>
